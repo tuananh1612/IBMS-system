2c2802ec9ff92f7a5926c5a25740b83dba248298
Elise Hein <elisehein@gmail.com>
2012-05-01 16:10:25 +0000

    Starting work on PlanJourney

--- PlanJourney.java
+++ PlanJourney.java
@@ -4,9 +4,34 @@
  * First creates a graph, then applies a shortest path algorithm
  * to it before adding timing.
  * 
+ * TO GET JOURNEY:
+ * First construct a PlanJourney object
+ * giving it the arguments int startStop, int endStop
+ * Date startTime, Date endTime
+ * Then get the journey[][] array with getJourney().
+ * 
+ * See doc http://goo.gl/Ezg7s for details
+ * 
  * Person responsible: Elise
  * 
  */
 public class PlanJourney {
-
+  
+  // Initialize the journey array
+  private int[][] journey;
+  
+  public PlanJourney(int startStop,
+                     int endStop,
+                     Date startTime,
+                     Date endTime) {
+    // get relevand services from DataManager
+    // create a graph with these services
+    // find shortest path
+    // populate array
+  
+  } // constructor
+  
+  public int[][] getJourney {
+    return journey;
+  } // getter method
 } // class
