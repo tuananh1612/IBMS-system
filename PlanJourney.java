476274d4a14163fb0acc05a51dcc35f0371dbe73
Elise Hein <elisehein@gmail.com>
2012-05-01 16:56:28 +0000

    Initial PlanJourney

--- PlanJourney.java
+++ PlanJourney.java
@@ -15,6 +15,9 @@
  * Person responsible: Elise
  * 
  */
+
+import java.util.ArrayList; 
+
 public class PlanJourney {
   
   // Initialize the journey array
@@ -24,10 +27,31 @@ public class PlanJourney {
                      int endStop,
                      Date startTime,
                      Date endTime) {
-    // get relevand services from DataManager
-    // create a graph with these services
-    // find shortest path
-    // populate array
+    
+    // Get a relevant list of services
+    int stops[] = {startStop, endStop};
+    ArrayList<Integer[]> services = new ArrayList<Integer[]>();
+    services = DataManager.getServicesFor(stops, startTime);
+   
+    // Create a graph with these services
+    ArrayList<BusStop> stopsGraph = new ArrayList<BusStop>();
+    stopsGraph = DataManager.createGraph(services);
+    
+    // Find shortest path in the graph
+    ShortestPath shortest = new ShortestPath(startStop,
+                                             endStop,
+                                             startTime,
+                                             endTime,
+                                             stopsGraph);
+    int[][] stopsInShortest = shortest.getStops();
+    int[] timesInShortest = shortest.getTimes();
+    
+    // Populate journey[][]
+    for (int i = 0; i < stopsInShortest.length; i++) {
+      journey[i][0] = stopsInShortest[i][0];
+      journey[i][1] = stopsInShortest[i][1];
+      journey[i][2] = timesInShortest[i];
+    }
   
   } // constructor
   
