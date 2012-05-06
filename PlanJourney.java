b90b81b4e9e2e2728423d121d21359509290234f
Elise Hein <elisehein@gmail.com>
2012-05-06 20:17:01 +0100

    mapping the ID's. now need to adjust the shortest path algorithm to deal with this

--- PlanJourney.java
+++ PlanJourney.java
@@ -1,14 +1,16 @@
 
 import java.util.Date;
 import java.util.ArrayList;
+import java.util.ListIterator;
+import java.util.Iterator;
 
 public class PlanJourney {
   
   // Initialize the journey array
   private int[][] journey;
   
-  public PlanJourney(int startStop,
-                     int endStop,
+  public PlanJourney(String startStop,
+                     String endStop,
                      Date startDate,
                      int time) {
     
@@ -17,18 +19,24 @@ public class PlanJourney {
 		manager.createRouteGraph();
     int[][] stopsGraph = manager.getRouteGraph();
     
+		/* Use the StopID array from DataManager
+		 * to map the graph ID-s to the correct bus stops */
+		int[][] IDmap = manager.getStopID();
+		ArrayList<String> StringMap = manager.getStopNames();
+		
+		int[] stringIndexes = findStringIndexes(StringMap, startStop, endStop);
+		int startStopInt = IDmap[stringIndexes[0]][0];
+		int endStopInt = IDmap[stringIndexes[1]][0];
+		
     // Find shortest path in the graph
-    ShortestPath shortest = new ShortestPath(startStop,
-                                             endStop,
+    ShortestPath shortest = new ShortestPath(startStopInt,
+                                             endStopInt,
                                              startDate,
                                              time,
                                              stopsGraph);
     int[][] stopsInShortest = shortest.getStops();
     int[] timesInShortest = shortest.getTimes();
 		
-		/* Use the StopID array from DataManager
-		 * to map the graph ID-s to the correct bus stops */
-		int[][] IDmap = manager.getStopID();
     
     // Populate journey[][]
     for (int i = 0; i < stopsInShortest.length; i++) {
@@ -38,7 +46,22 @@ public class PlanJourney {
     }
   
   } // constructor
-  
+	
+	private int[] findStringIndexes(ArrayList<String> map, String a, String b) {
+		int[] indexes = new int[2];
+		int aInt = -1;
+		int bInt = -1;
+		for (int i = 0; i < map.size(); i++) {
+			if (map.get(i) == a)
+				aInt = i;
+			if (map.get(i) == b)
+				bInt = i;
+		}
+		indexes[0] = aInt;
+		indexes[1] = bInt;
+		return indexes;
+	} // findStringIndexes
+	
   public int[][] getJourney() {
     return journey;
   } // getter method 
