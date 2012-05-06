5ac16e863a481b98d49a57f060866207a6ab3064
Tuan Anh <tuananh1612@gmail.com>
2012-05-07 05:12:55 +0700

    Add convertPath method. Minor change to main algorithm

--- PathTiming.java
+++ PathTiming.java
@@ -1,5 +1,8 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Date;
+
+import javax.swing.text.DefaultEditorKit.CopyAction;
 
 
 public class PathTiming {
@@ -7,6 +10,8 @@ public class PathTiming {
 	public Date startTime;
 	public int hour;
 	public ArrayList<int[]> shortestPath;
+	public ArrayList<int[]> workingPath;
+	public ArrayList<int[]> backUpPath;
 	//The format of int array in shortestPath is 2 element: 
 	//the stop and the route use that number(?)
 	public int numberOfNodes;
@@ -22,6 +27,7 @@ public class PathTiming {
 		startTime = requiredTime;
 		hour = requiredHour;
 		kind = TimetableInfo.timetableKind(startTime);
+		backUpPath.addAll(shortestPath);
 	}
 	
 	//Get the timing of the route
@@ -38,6 +44,7 @@ public class PathTiming {
 		//Return the timeArray.
 		//Repeat until the end of the path.
 		//May record the route order in an array.
+		convertPath();
 		int routeNumber = shortestPath.size() / 2;
 		timeArray = new int[shortestPath.size()];
 		serviceArray = new int[shortestPath.size()];
@@ -96,14 +103,16 @@ public class PathTiming {
 			//Check if the start point is in the timingPoint array
 			boolean startIsIn = false;
 			boolean endIsIn = false;
+			startLocation = start;
 			for (int j = 0; j < timingPoint.length; j ++) {
 				if (timingPoint[j] == start) {
 					startIsIn = true;
 					startLocation = j;
 				}
-				if (timingPoint[j] == end) {
+				if ((startLocation < j) &&(timingPoint[j] == end)) {
 					endIsIn = true;
 					endLocation = j;
+					break;
 				}
 			}
 			if ((startIsIn) && (endIsIn)) {
@@ -135,5 +144,128 @@ public class PathTiming {
 	
 	public int[] getServiceArray() {
 		return serviceArray;
+	}
+	
+	public void convertPath() {
+		//Convert the stop IDs to suitable IDs for processing
+		int noOfRoute = shortestPath.size() / 2;
+		DataManager manager = new DataManager(startTime);
+		ArrayList<String> stopName = manager.getStopNames();
+		int[][] stopIDs = manager.getStopID();
+		int[] routes = BusStopInfo.getRoutes();
+		int routeNumber;
+		ArrayList<String> temp = new ArrayList<String>();
+		for (int i = 0; i < shortestPath.size(); i ++) {
+			//Get the name of the stop
+			String name = BusStopInfo.getFullName((shortestPath.get(i))[0]);
+			temp.add(name);
+		}
+		//Resolve stop IDs in pair
+		for (int i = 0; i < noOfRoute; i ++) {
+			int startIndex = stopName.indexOf(temp.get(i * 2));
+			int endIndex = stopName.indexOf(temp.get(i * 2 + 1));
+			ArrayList<Integer> temp1 = new ArrayList<Integer>();
+			ArrayList<Integer> temp2 = new ArrayList<Integer>();
+			//Get the suitable IDs for start and end points
+			for (int j = 0; j < 10; j ++) {
+				if (BusStopInfo.isTimingPointOnRoute(stopIDs[startIndex][j], 
+						(shortestPath.get(i * 2))[1])) {
+					temp1.add(stopIDs[startIndex][j]);
+				}
+			}
+			for (int j = 0; j < 10; j ++) {
+				if (BusStopInfo.isTimingPointOnRoute(stopIDs[endIndex][j], 
+						(shortestPath.get(i * 2 + 1))[1])) {
+					temp2.add(stopIDs[endIndex][j]);
+				}
+			}
+			//Check and register final ID to shortestPath
+			int[] startEntry = new int[2];
+			int[] endEntry = new int[2];
+			startEntry[1] = (shortestPath.get(i * 2))[1];
+			endEntry[1] = (shortestPath.get(i * 2 + 1))[1];
+			if (temp1.size() == 1) {
+				startEntry[0] = temp1.get(0);
+			}
+			if (temp2.size() == 1) {
+				endEntry[0] = temp2.get(0);
+			}
+			//There are more than one IDs
+			if ((temp1.size() > 1) && (temp2.size() == 1)) {
+				int k = 0;
+				while (temp1.get(k) > temp2.get(0)) {
+					if (BusStopInfo.getNextStop(temp1.get(k), startEntry[1]) != 0) {
+						startEntry[0] = temp1.get(k);
+						break;
+					}
+					k ++;
+				}
+			}
+			else if ((temp1.size() == 1) && (temp2.size() > 1)) {
+				int k = 0;
+				while (temp1.get(0) > temp2.get(k)) {
+					k ++;
+				}
+				endEntry[0] = temp2.get(k);
+			}
+			else {
+				boolean found = false;
+				for (int t = 0; t < temp1.size(); t ++) {
+					for (int k = 0; k < temp2.size(); k ++) {
+						if (isMatched(startEntry[1], temp1.get(t), temp2.get(k))) {
+							startEntry[0] = temp1.get(t);
+							endEntry[0] = temp2.get(k);
+							break;
+						}
+					}
+					if (found) {
+						break;
+					}
+				}
+			}
+			workingPath.add(startEntry);
+			workingPath.add(endEntry);
+		}
+		shortestPath.clear();
+		shortestPath.addAll(workingPath);
+	}
+	
+	public boolean isMatched(int routeNumber, int start, int end) {
+		boolean result;
+		boolean found = false;
+		int startIndex, endIndex;
+		if (end < start) {
+			result = false;
+		}
+		else {
+			//Check if there is a service run from start to end
+			int[] services = TimetableInfo.getServices(routeNumber, kind);
+			for (int i = 0; i < services.length; i ++) {
+				int[] sequence = TimetableInfo.getTimingPoints(routeNumber, 
+						kind, services[i]);
+				startIndex = start;
+				found = false;
+				for (int k = 0; k < sequence.length; k ++) {
+					if (sequence[k] == start) {
+						startIndex = k;
+					}
+					if ((sequence[k] == end) && (k > startIndex)) {
+						endIndex = k;
+						found = true;
+						break;
+					}
+				}
+				if (found) {
+					break;
+				}
+			}
+			if (found) {
+				result = true;
+			}
+			else {
+				result = false;
+			}
+		}
+		return result;
 	}
 }
